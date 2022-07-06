101288d8c5eb2851beaf633b2cda621bb5c652e3
Diego Lipinski <diegocastroh20@gmail.com>
2022-07-06 19:38:21 -0300

    minor

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -44,7 +44,7 @@
                                 </td>
 
                                 <td class="whitespace-nowrap px-3 py-2 text-sm text-gray-500 border-l">
-                                    {{ $client->email }}
+                                    {{ $client->email ?? '-' }}
                                 </td>
 
                                 <td class="whitespace-nowrap px-3 py-2 text-sm text-gray-500 border-l">
--- resources/views/sellers/index.blade.php
+++ resources/views/sellers/index.blade.php
@@ -40,7 +40,7 @@
                                 </td>
 
                                 <td class="whitespace-nowrap px-3 py-2 text-sm text-gray-500 border-l">
-                                    {{ $seller->email }}
+                                    {{ $seller->email ?? '-' }}
                                 </td>
 
                                 <td class="whitespace-nowrap px-3 py-2 text-sm border-l">
