c88461fd7f85a6333ae76000e4c4401c0d99d751
Diego Lipinski <diegocastroh20@gmail.com>
2024-09-22 11:24:31 -0300

    minor

--- app/Models/SiteType.php
+++ app/Models/SiteType.php
@@ -5,11 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Pivot;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use OwenIt\Auditing\Contracts\Auditable;
 
 class SiteType extends Pivot implements Auditable
 {
     use HasFactory;
+    use SoftDeletes;
     use \OwenIt\Auditing\Auditable;
 
     /**
